16bf8b70a38ff6ceaef207570ebdf981ea906218
Anthony Fumagalli <anthony.fumagalli@gmail.com>
2016-08-23 15:52:25 -0700

    Modified data objects to match schema

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -11,17 +11,16 @@ var bio = {
         twitter: "@roastedTofu",
         location: "San Francisco, CA"
     },
-    welcomeMessage: "Welcome to my resume",
+    welcomeMessage: "I'm a full stack engineer with a passion for education. I say my passion is education because I believe education to be one of our most important human rights - and improving access to high quality education is one of our most important social justice challenges today. I believe education technology is fundamental to this cause and that well developed technology can not only increase accessibility, but also change the way people learn entirely.",
     skills: [
         "Python", "JavaScript", "C", "Google App Engine", "PostgreSQL", "Linux server administration", "Configuration management"
     ],
-    bioPic: "images/anthony.jpg",
+    biopic: "images/anthony.jpg",
     display: function displayBio() {
-        var formattedName = HTMLheaderName.replace("%data%", bio.name);
-        $("#header").append(formattedName);
         var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-        $("#header").append(formattedRole);
-
+        $("#header").prepend(formattedRole);
+        var formattedName = HTMLheaderName.replace("%data%", bio.name);
+        $("#header").prepend(formattedName);
         var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
         $("#topContacts").append(formattedMobile);
         $("#footerContacts").append(formattedMobile);
@@ -37,12 +36,10 @@ var bio = {
         var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
         $("#topContacts").append(formattedLocation);
         $("#footerContacts").append(formattedLocation);
-
-        var formattedPic = HTMLbioPic.replace("%data%", bio.bioPic);
+        var formattedPic = HTMLbioPic.replace("%data%", bio.biopic);
         $("#header").append(formattedPic);
         var formattedWelcome = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
         $("#header").append(formattedWelcome);
-
         $("#header").append(HTMLskillsStart);
         for (var i = 0; i < bio.skills.length; i++) {
             var formattedSkill = HTMLskills.replace("%data%", bio.skills[i]);
@@ -58,43 +55,32 @@ var work = {
             title: "Course manager",
             location: "Mountain View, CA",
             dates: "July 2015 - present",
-            description: "I support students in Udacity's Intro to Programming Nanodegree by hosting \
-                live office hours, webcasts, and tutoring sessions. I am also the point person on content \
-                for the Nanodegree and work with course developers and project managers to create new \
-                content and improve existing course materials."
+            description: "I support students in Udacity's Intro to Programming Nanodegree by hosting live office hours, webcasts, and tutoring sessions. I am also the point person on content for the Nanodegree and work with course developers and project managers to create new content and improve existing course materials."
         },
         {
             employer: "Dwight Hall at Yale",
             title: "Co-coordinator, Public School Interns",
             location: "New Haven, CT",
             dates: "August 2012 - May 2015",
-            description: "After serving as a Public School Intern for two years by volunteering in classrooms \
-            and conducting after school tutoring, I then coordinated the program during my senior year. We \
-            had 14 interns working in 14 New Haven Public Schools with a focus on engaging students with \
-            current issues in public education."
+            description: "After serving as a Public School Intern for two years by volunteering in classrooms and conducting after school tutoring, I then coordinated the program during my senior year. We had 14 interns working in 14 New Haven Public Schools with a focus on engaging students with current issues in public education."
         },
         {
             employer: "Recovery School District",
             title: "Education policy intern",
             location: "New Orleans, LA",
             dates: "June 2014 - August 2014",
-            description: "I worked with Executive Director of Policy Holly Reid to prepare the Recovery \
-            School District for its first year as an all-charter school district. My projects included \
-            writing policy reports on early childhood education and tracking school renovation funding."
+            description: "I worked with Executive Director of Policy Holly Reid to prepare the Recovery School District for its first year as an all-charter school district. My projects included writing policy reports on early childhood education and tracking school renovation funding."
         }
     ],
     display: function displayWork() {
         for (var i = 0; i < work.jobs.length; i++) {
             $("#workExperience").append(HTMLworkStart);
-
             var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[i].employer);
             var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[i].title);
             var formattedEmployerTitle = formattedEmployer + formattedTitle;
             $(".work-entry:last").append(formattedEmployerTitle);
-
             var formattedDates = HTMLworkDates.replace("%data%", work.jobs[i].dates);
             $(".work-entry:last").append(formattedDates);
-
             var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[i].description);
             $(".work-entry:last").append(formattedDescription);
         }
@@ -106,8 +92,7 @@ var projects = {
         {
             title: "Intro to Programming Notes",
             dates: "July 2015",
-            description: "My first webpage, a collection of notes for Udacity's Intro to Programming \
-            Nanodegree <a href='http://ipnd-notes.appspot.com/'>Click here to check it out</a>",
+            description: "My first webpage, a collection of notes for Udacity's Intro to Programming Nanodegree <a href='http://ipnd-notes.appspot.com/'>Click here to check it out</a>",
             images: [
                 "images/ipnd.png"
             ]
@@ -116,16 +101,12 @@ var projects = {
     display: function displayProjects() {
         for (var i = 0; i < projects.projects.length; i++) {
             $("#projects").append(HTMLprojectStart);
-
             var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[i].title);
             $(".project-entry:last").append(formattedTitle);
-
             var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[i].dates);
             $(".project-entry:last").append(formattedDates);
-
             var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[i].description);
             $(".project-entry:last").append(formattedDescription);
-
             for (var j = 0; j < projects.projects[i].images.length; j++)
                 var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[i].images[j]);
                 $(".project-entry:last").append(formattedImage);
@@ -139,7 +120,7 @@ var education = {
             name: "Yale University",
             location: "New Haven, CT",
             degree: "Bachelor of Arts",
-            major: "Computer Science & Mathematics",
+            majors: ["Computer Science & Mathematics"],
             dates: "2015",
             url: "http://www.yale.edu"
         }
@@ -165,15 +146,14 @@ var education = {
             var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools[i].degree);
             var formattedNameDegree = formattedName + formattedDegree;
             $(".education-entry:last").append(formattedNameDegree);
-
             var formattedDates = HTMLschoolDates.replace("%data%", education.schools[i].dates);
             $(".education-entry:last").append(formattedDates);
-
             var formattedLocation = HTMLschoolLocation.replace("%data%", education.schools[i].location);
             $(".education-entry:last").append(formattedLocation);
-
-            var formattedMajor = HTMLschoolMajor.replace("%data%", education.schools[i].major);
-            $(".education-entry:last").append(formattedMajor);
+            for (var j = 0; j < education.schools[i].majors.length; j++) {
+                var formattedMajor = HTMLschoolMajor.replace("%data%", education.schools[i].majors[j]);
+                $(".education-entry:last").append(formattedMajor);
+            }
         }
         $("#education").append(HTMLonlineClasses);
         for (i = 0; i < education.onlineCourses.length; i++) {
@@ -183,7 +163,6 @@ var education = {
             var formattedSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[i].school);
             var formattedTitleSchool = formattedTitle + formattedSchool;
             $(".education-entry:last").append(formattedTitleSchool);
-
             var formattedDates = HTMLonlineDates.replace("%data%", education.onlineCourses[i].dates);
             $(".education-entry:last").append(formattedDates);
         }
